8cb039e1012f74c9110eef387d55fbc003ddff68
mallicksm <mallicksm@gmail.com>
2024-04-11 08:39:04 -0700

    minimal home

--- corp_settings.sh
+++ corp_settings.sh
@@ -10,6 +10,10 @@
 # Link this file to $HOME 
 #    ln -s $0/corp_settings.sh $HOME/
 #===============================================================================
-export corp=home
 source ~/dotfiles/utils/bash_snippets.sh 2>/dev/null;# must call for modpath
+#-------------------------------------------------------------------------------
+# Establish rootpath
+export corp=home
 modpath ~/tools/nvim-macos/bin b
+modpath ~/.local/bin b
+export ZELLIJ_CONFIG_FILE=~/dotfiles/initrc/zellij/config.mac.kdl
