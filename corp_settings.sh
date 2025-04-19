7dc153edec2ffa7ec71dd30ceb7cd937804506fa
mallicksm <mallicksm@gmail.com>
2025-04-19 16:10:02 -0700

    mods

--- corp_settings.sh
+++ corp_settings.sh
@@ -14,8 +14,12 @@ source ~/dotfiles/utils/bash_snippets.sh 2>/dev/null;# must call for modpath
 #-------------------------------------------------------------------------------
 # Establish rootpath
 export corp=home
-modpath ~/tools/nvim-macos/bin b
+modpath ~/tools/nvim-macos-arm64/bin b
 modpath ~/tools/meld/bin b
 modpath ~/.local/bin b
+modpath /opt/homebrew/Cellar/llvm/17.0.6_1/bin/ b
+modpath /opt/X11/bin/ b
+modpath ~/tools/verible/verible-v0.0-3648-g5ef1624a-macOS/bin b
 tools=~/tools
+tools=~/proj
 dotfiles=~/dotfiles
